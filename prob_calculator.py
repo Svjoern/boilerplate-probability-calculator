438df6af3d063caa667653ba4bbf520818528861
Sven <Sven_Kuehne@hotmail.de>
2022-02-09 06:49:37 +0000

    .

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,6 +3,30 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+  content = []
+
+  def __init__(self, *args, **kwargs):
+    self.addContent(args, kwargs)
+
+  # def __init__(self, blue=0, red=0, green=0):
+  #   self.blue = blue
+  #   self.red = red
+  #   self.green = green
+    
+  def addContent(self,*args, **kwargs):
+    for elem in args:
+      self.content.append(elem.kwargs)
+
+  def draw(self, val):
+    if val >= len(self.content):
+      return self.content
+
+    for i in range(val):
+      return random.sample(self.content, val)
+      # print(random.sample(self.content, val))
+
+
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+  pass
