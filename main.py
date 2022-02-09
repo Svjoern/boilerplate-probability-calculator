fa72d028d098404df7792a4abfbb8617d77c5826
Sven <Sven_Kuehne@hotmail.de>
2022-02-09 13:38:58 +0000

    start

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-# This entrypoint file to be used in development. Start by reading README.md
+ # This entrypoint file to be used in development. Start by reading README.md
 import prob_calculator
 from unittest import main
 
 prob_calculator.random.seed(95)
+prob_calculator.random.seed(1337)
 hat = prob_calculator.Hat(blue=4, red=2, green=6)
+
+# print(hat.draw(6))
+
 probability = prob_calculator.experiment(
     hat=hat,
     expected_balls={"blue": 2,
                     "red": 1},
     num_balls_drawn=4,
-    num_experiments=3000)
+    # num_experiments=3000)
+    num_experiments=3)
 print("Probability:", probability)
 
 # Run unit tests automatically
--- prob_calculator.py
+++ prob_calculator.py
@@ -2,31 +2,62 @@ import copy
 import random
 # Consider using the modules imported above.
 
+
 class Hat:
-  content = []
+    contents = []
+    Balls_total = 0
 
-  def __init__(self, *args, **kwargs):
-    self.addContent(args, kwargs)
+    def __init__(self, **ball_colors_numbr):
+        self.addContent(ball_colors_numbr)
+        # print(self,self.contents)
 
-  # def __init__(self, blue=0, red=0, green=0):
-  #   self.blue = blue
-  #   self.red = red
-  #   self.green = green
-    
-  def addContent(self,*args, **kwargs):
-    for elem in args:
-      self.content.append(elem.kwargs)
+    def addContent(obj, ball_colors_numbr):
+        for ball in ball_colors_numbr:
+            for i in range(ball_colors_numbr[ball]):
+                obj.contents.append(ball)
+            # print(obj,ball,"\t=" ,ball_colors_numbr[ball])
+            obj.Balls_total += ball_colors_numbr[ball]
+        # print(self.contents)
+        # print(self.Balls_total)
 
-  def draw(self, val):
-    if val >= len(self.content):
-      return self.content
-
-    for i in range(val):
-      return random.sample(self.content, val)
-      # print(random.sample(self.content, val))
-
-
+    def draw(self, val):
+        drawn_balls = []
+        if val >= len(self.contents):
+            drawn_balls = self.contents
+            self.contents.clear()
+            return self.contents
+        else:
+            for i in range(val):
+                index = random.randint(0,
+                                       len(self.contents) - 1)  # print(index)
+                drawn_balls.append(self.contents.pop(index))
+        # print(drawn_balls, self.contents)
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-  pass
+    # test_hat = hat
+    target = []
+    target_total = 0
+    probability = 0
+
+    for ball in expected_balls:
+        for i in range(expected_balls[ball]):
+            target.append(ball)
+            # print(obj,ball,"\t=" ,ball_colors_numbr[ball])
+        target_total += expected_balls[ball]
+
+    # print("hallo", test_hat, target, target_total)
+
+    for experiment_number in range(num_experiments):
+        test_hat = copy.copy(hat)
+        test_target = expected_balls
+        tmp = test_hat.draw(num_balls_drawn)
+        print(tmp)
+
+        for ball in test_target:
+            for i in range(expected_balls[ball]):
+                target.append(ball)
+                # print(obj,ball,"\t=" ,ball_colors_numbr[ball])
+            target_total += expected_balls[ball]
+
+    return probability
