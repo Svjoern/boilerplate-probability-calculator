e31260b523fcb548c326a8607ca1132c4e669e31
Sven <Sven_Kuehne@hotmail.de>
2022-02-09 15:33:14 +0000

    nuisance

--- main.py
+++ main.py
@@ -3,19 +3,24 @@ import prob_calculator
 from unittest import main
 
 prob_calculator.random.seed(95)
-prob_calculator.random.seed(1337)
+# prob_calculator.random.seed(1337)
 hat = prob_calculator.Hat(blue=4, red=2, green=6)
+
+# print("num_balls_drawn=4", "num_experiments=1000")
+# hat = prob_calculator.Hat(blue=3,red=2,green=6)
+# expected_balls={"blue":2,"green":1}
+# num_balls_drawn=4
+# num_experiments=1000
+
+# print("expected_balls:\t",expected_balls)
+# probability = prob_calculator.experiment(hat, expected_balls, num_balls_drawn, num_experiments)
+# print("Probability:", probability, "expected = 0.272")
 
 # print(hat.draw(6))
 
-probability = prob_calculator.experiment(
-    hat=hat,
-    expected_balls={"blue": 2,
-    # expected_balls={"green": 2,
-                    "red": 1},
-    num_balls_drawn=3,
-    num_experiments=3000)
-    # num_experiments=3)
+print("expected_balls:  {'blue': 2, 'red': 1}")
+probability = prob_calculator.experiment(hat,{"blue": 2,"red": 1},11,10) 
+    # num_experiments=3000)
 print("Probability:", probability)
 
 # Run unit tests automatically
--- prob_calculator.py
+++ prob_calculator.py
@@ -4,39 +4,46 @@ import random
 
 class Hat:
     contents = []
-    Balls_total = 0
+    # Balls_total = 0
 
     def __init__(self, **ball_colors_numbr):
         self.addContent(ball_colors_numbr)
-        # print(self,self.contents)
+        print("hat.contents:\t", ball_colors_numbr)
 
     def addContent(self, ball_colors_numbr):
+        self.contents.clear()
+        self.Balls_total = 0
+
         for ball in ball_colors_numbr:
             for i in range(ball_colors_numbr[ball]):
                 self.contents.append(ball)
-            # print(obj,ball,"\t=" ,ball_colors_numbr[ball])
+                # print(obj,ball,"\t=" ,ball_colors_numbr[ball])
             self.Balls_total += ball_colors_numbr[ball]
-        # print(self.contents)
-        # print(self.Balls_total)
+        # print(self.contents, self.Balls_total)
 
     def draw(self, val):
         drawn_balls = []
         if val >= len(self.contents):
+            # print("return entire content")
             drawn_balls = copy.deepcopy(self.contents)
             self.contents.clear()
-            return drawn_balls
+            # return drawn_balls
         else:
             for i in range(val):
-                index = random.randint(0, len(self.contents) - 1)  # print(index)
+                index = random.randint(0,len(self.contents)-1)  # print(index)
                 drawn_balls.append(self.contents.pop(index))
         # print(drawn_balls, self.contents)
         return drawn_balls
 
 
+
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    if num_balls_drawn == len(hat.contents):
+        return 1.0
+
     test_hat = copy.deepcopy(hat)
-    P = 0 # probability
-    M = 0 # succesful_draws
+    P = 0  # probability
+    M = 0  # succesful_draws
     N = num_experiments
 
     # for ball in expected_balls:
@@ -47,30 +54,31 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     # print("hallo", test_hat, target, target_total)
 
     for experiment_number in range(num_experiments):
-      test_target_total = 0
-      test_target = copy.deepcopy(expected_balls) # probably directly copy it cos dictionary
-      test_hat.contents = copy.deepcopy(hat.contents)
+        test_target_total = 0
+        test_target = copy.deepcopy(expected_balls)  # probably directly copy it cos dictionary
+        test_hat.contents = copy.deepcopy(hat.contents)     
+        current_Draw = test_hat.draw(num_balls_drawn)
+        # print("exp:",experiment_number,current_Draw, test_target)
 
-      current_Draw = test_hat.draw(num_balls_drawn)
-      # print("exp:",experiment_number,current_Draw, test_target)
+        for ball in test_target:
+            # print("\nfind", ball, "ball", test_target[ball], "times \n")
+            test_target_total += test_target[ball]
 
-      for ball in test_target:
-        # print("\nfind", ball, "ball", test_target[ball], "times \n")
-        test_target_total += test_target[ball]
+            for i in range(test_target[ball]):
+                for current_Draw_index, current_Draw_ball in enumerate(
+                        current_Draw):
+                    if current_Draw_ball == ball:
+                        # print("hit:", current_Draw_ball, test_target[ball])
+                        # print("Current_Draw_index", current_Draw_index, "color",current_Draw_ball, "\n")
+                        current_Draw[current_Draw_index] = ""
+                        test_target[ball] -= 1
+                        test_target_total -= 1
 
-        for i in range(test_target[ball]):
-          for current_Draw_index, current_Draw_ball in enumerate(current_Draw):
-            if current_Draw_ball == ball:
-              # print("hit:", current_Draw_ball, test_target[ball])
-              # print("Current_Draw_index", current_Draw_index, "color",current_Draw_ball, "\n")
-              current_Draw[current_Draw_index] = ""
-              test_target[ball] -= 1
-              test_target_total -= 1
-                
-      if test_target_total == 0:
-        M += 1
-      # print("---", M)
+        # print("current_Draw",current_Draw)
+        if test_target_total == 0:
+            M += 1
+        # print("---", M)
 
-    P = M/N
-    print("M",M,"N",N)
+    P = M / N
+    print("M", M, "N", N)
     return P
